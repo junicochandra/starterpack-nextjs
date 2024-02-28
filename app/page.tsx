30fc05ed7a02676adbe142f0a08ba0a9598378b6
Junico Dwi Chandra <junicodwi.chandra@gmail.com>
2024-02-28 11:13:32 +0700

    add template

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import Feed from "@components/Feed";
+
 export default function Home() {
   return (
     <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">Discover & Share</h1>
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptatibus perferendis ut perspiciatis necessitatibus iure veritatis a deserunt veniam dolorem?
+      </p>
+
+      <Feed />
     </section>
   )
 }
---
+++ components/Nav.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from 'react';
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
+export default function Nav() {
+  const isUserLoggedIn = true;
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src='/assets/images/logo.svg'
+          alt='logo'
+          width={30}
+          height={30}
+          className='object-contain'
+        />
+        <p className="logo_text">Junico</p>
+      </Link>
+
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden">
+        { isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">Create Post</Link>
+            <button type="button" onClick={signOut} className="outline_btn">Sign Out</button>
+            <Link href="/profile">
+              <Image 
+                src="/assets/images/logo.svg"
+                alt="profile"
+                width={37}
+                height={37}
+                className="rounded-full"
+              />
+            </Link>
+          </div>
+        ): (
+          <></>
+        )}
+      </div>
+    </nav>
+  )
+}
